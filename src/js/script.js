e7a15c5981010c57382e6c8547e8ee09dee5a1e6
Fabio "Fabiane" Cristiano <100153999+Hiaosh@users.noreply.github.com>
2024-02-27 19:49:16 -0300

    Atualizando projeto básico de JS
    Nesta atualização eu mudei as propriedades de rolagem de pagina, e a interação da navbar, deixando de ser fixa no topo da pagina para ficar flutuando durante a rolagem da pagina.

--- src/js/script.js
+++ src/js/script.js
@@ -21,10 +21,4 @@ const elementoPlataformas = document.querySelector(".btn-platform .platform");
 
 botao.addEventListener("click", () => {
     elementoPlataformas.classList.toggle("ativo");
-
-    // if(elementoPlataformas.classList.contains("ativo")){
-    //     elementoPlataformas.classList.remove("ativo");
-    // }else{
-    //     elementoPlataformas.classList.add("ativo");
-    // }
 });
